cc891cd78b08dbeb9c24d64f9acb8a83a399c4bc
MrZak-dev <zakaria.guenna@gmail.com>
2019-04-13 21:28:42 +0100

    first try

--- config.py
+++ config.py
@@ -4,3 +4,6 @@ def can_build(env,plat):
 def configure(env):
     if env['platform'] == 'android':
         env.android_add_java_dir("src")
+        #env.android_add_maven_repository("https://repo.maven.apache.org/maven2")
+        env.android_add_dependency("implementation 'com.android.support:recyclerview-v7:25.3.1'")
+        env.android_add_dependency("implementation 'com.facebook.android:audience-network-sdk:5.+'")
--- src/FacebookAudienceNetwork.java
+++ src/FacebookAudienceNetwork.java
@@ -2,20 +2,63 @@ package org.godotengine.godot;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.content.Context;
 import com.godot.game.R;
 import javax.microedition.khronos.opengles.GL10;
 import com.facebook.ads.*;
+import android.util.Log;
+
 
 
 public class FacebookAudienceNetwork extends Godot.SingletonBase {
 
-    //protected Activity appActivity;
-    //protected Context appContext;
+    protected Activity appActivity;
+    protected Context appContext;
     //private int instanceId = 0;
-    //private InterstitialAd myInterstitialAd;
+    private InterstitialAd myInterstitialAd;
 
-    //myInterstitialAd = new InterstitialAd(this,"285548149034555_285553599034010");
+    public void InterstitiaLoader(){
+        myInterstitialAd = new InterstitialAd(this.appContext,"285548149034555_285553599034010");
+        myInterstitialAd.setAdListener(new InterstitialAdListener() {
+            @Override
+            public void onInterstitialDisplayed(Ad ad) {
+                // Interstitial ad displayed callback
+                Log.e("FAN", "Interstitial ad displayed.");
+            }
+            @Override
+            public void onInterstitialDismissed(Ad ad) {
+                // Interstitial dismissed callback
+                Log.e("FAN", "Interstitial ad dismissed.");
+            }
+            @Override
+            public void onError(Ad ad, AdError adError) {
+                // Ad error callback
+                Log.e("FAN", "Interstitial ad failed to load: " + adError.getErrorMessage());
+            }
 
+            @Override
+            public void onAdLoaded(Ad ad) {
+                // Interstitial ad is loaded and ready to be displayed
+                Log.d("FAN", "Interstitial ad is loaded and ready to be displayed!");
+                // Show the ad
+                myInterstitialAd.show();
+            }
+            @Override
+            public void onAdClicked(Ad ad) {
+                // Ad clicked callback
+                Log.d("FAN", "Interstitial ad clicked!");
+            }
+
+            @Override
+            public void onLoggingImpression(Ad ad) {
+                // Ad impression logged callback
+                Log.d("FAN", "Interstitial ad impression logged!");
+            }
+        });
+    
+    }
+
+    
     public int add(int a , int b) {
         // a function to bind
         return a+b;
@@ -32,9 +75,9 @@ public class FacebookAudienceNetwork extends Godot.SingletonBase {
 
     public FacebookAudienceNetwork(Activity p_activity) {
         //register class name and functions to bind
-        registerClass("FacebookAudienceNetwork", new String[]{"add"});
-        //this.appActivity = p_activity;
-        //this.appContext = appActivity.getApplicationContext();
+        registerClass("FacebookAudienceNetwork", new String[]{"add","InterstitiaLoader"});
+        this.appActivity = p_activity;
+        this.appContext = appActivity.getApplicationContext();
         // you might want to try initializing your singleton here, but android
         // threads are weird and this runs in another thread, so to interact with Godot you usually have to do
         
