67ae0609baec26e914a9d8bea450c66886d7f801
MrZak-dev <zakaria.guenna@gmail.com>
2019-04-18 22:20:56 +0100

    interstitial ad now is working call InterstitialLoader() to show it

--- config.py
+++ config.py
@@ -5,5 +5,5 @@ def configure(env):
     if env['platform'] == 'android':
         env.android_add_java_dir("src")
         #env.android_add_maven_repository("https://repo.maven.apache.org/maven2")
-        env.android_add_dependency("implementation 'com.android.support:recyclerview-v7:25.3.1'")
-        env.android_add_dependency("implementation 'com.facebook.android:audience-network-sdk:5.+'")
+        env.android_add_dependency("compile 'com.android.support:recyclerview-v7:27.1.1'")
+        env.android_add_dependency("compile 'com.facebook.android:audience-network-sdk:5.+'")
--- src/FacebookAudienceNetwork.java
+++ src/FacebookAudienceNetwork.java
@@ -18,43 +18,51 @@ public class FacebookAudienceNetwork extends Godot.SingletonBase {
     private InterstitialAd myInterstitialAd;
 
     public void InterstitiaLoader(){
+        AudienceNetworkAds.initialize(this.appContext);
         myInterstitialAd = new InterstitialAd(this.appContext,"285548149034555_285553599034010");
-        myInterstitialAd.setAdListener(new InterstitialAdListener() {
-            @Override
-            public void onInterstitialDisplayed(Ad ad) {
-                // Interstitial ad displayed callback
-                Log.e("FAN", "Interstitial ad displayed.");
-            }
-            @Override
-            public void onInterstitialDismissed(Ad ad) {
-                // Interstitial dismissed callback
-                Log.e("FAN", "Interstitial ad dismissed.");
-            }
-            @Override
-            public void onError(Ad ad, AdError adError) {
-                // Ad error callback
-                Log.e("FAN", "Interstitial ad failed to load: " + adError.getErrorMessage());
-            }
-
-            @Override
-            public void onAdLoaded(Ad ad) {
-                // Interstitial ad is loaded and ready to be displayed
-                Log.d("FAN", "Interstitial ad is loaded and ready to be displayed!");
-                // Show the ad
-                myInterstitialAd.show();
-            }
-            @Override
-            public void onAdClicked(Ad ad) {
-                // Ad clicked callback
-                Log.d("FAN", "Interstitial ad clicked!");
-            }
-
-            @Override
-            public void onLoggingImpression(Ad ad) {
-                // Ad impression logged callback
-                Log.d("FAN", "Interstitial ad impression logged!");
+        myInterstitialAd.loadAd();
+        appActivity.runOnUiThread(new Runnable() {
+            public void run() {
+                
+                myInterstitialAd.setAdListener(new InterstitialAdListener() {
+                    @Override
+                    public void onInterstitialDisplayed(Ad ad) {
+                        // Interstitial ad displayed callback
+                        Log.e("FAN", "Interstitial ad displayed.");
+                    }
+                    @Override
+                    public void onInterstitialDismissed(Ad ad) {
+                        // Interstitial dismissed callback
+                        Log.e("FAN", "Interstitial ad dismissed.");
+                    }
+                    @Override
+                    public void onError(Ad ad, AdError adError) {
+                        // Ad error callback
+                        Log.e("FAN", "Interstitial ad failed to load: " + adError.getErrorMessage());
+                    }
+        
+                    @Override
+                    public void onAdLoaded(Ad ad) {
+                        // Interstitial ad is loaded and ready to be displayed
+                        Log.d("FAN", "Interstitial ad is loaded and ready to be displayed!");
+                        // Show the ad
+                        myInterstitialAd.show();
+                    }
+                    @Override
+                    public void onAdClicked(Ad ad) {
+                        // Ad clicked callback
+                        Log.d("FAN", "Interstitial ad clicked!");
+                    }
+        
+                    @Override
+                    public void onLoggingImpression(Ad ad) {
+                        // Ad impression logged callback
+                        Log.d("FAN", "Interstitial ad impression logged!");
+                    }
+                }); 
             }
         });
+        
     
     }
 
